2177b961ae4437dff4db3fa6f650c425f79d0ee0
AbduHosein <hoseina@bc.edu>
2023-01-23 00:03:29 -0500

    Custom forms and CSS

--- main/views.py
+++ main/views.py
@@ -7,6 +7,25 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 def index(response, id):
     todo = ToDoList.objects.get(id=id)
+    
+    if response.method == "POST":
+        if response.POST.get("save"):
+            for item in todo.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                
+                item.save()
+        
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new_text")
+
+            if len(txt) > 2:
+                todo.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid")
+
     return render(response, 'main/list.html', {"todo": todo})
 
 def home(response):
